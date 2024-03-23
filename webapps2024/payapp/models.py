300a5447c7e4b2a979cb77833d39599c779cb547
mgcrons <mgcrons@gmail.com>
2024-03-23 16:18:01 +0100

    build: created models, for users, wallet, transaction and notifications. Also customized auth user model

--- webapps2024/payapp/models.py
+++ webapps2024/payapp/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Wallet(models.Model):
+    """
+    CustomUser Wallet Table
+    """
+
+    user = models.ForeignKey(
+        "register.customuser",
+        on_delete=models.DO_NOTHING,
+        verbose_name=_("Wallet Owner"),
+        null=True,
+        related_name="wallet",
+    )
+    balance = models.PositiveIntegerField(default=1000)
+    account_type = models.CharField(
+        max_length="20",
+        choices=(
+            (
+                "gbp",
+                "GBP",
+            ),
+            (
+                "usd",
+                "USD",
+            ),
+        ),
+    )
+
+    def __str__(self):
+        return "{} Wallet".format(self.user.email)
+
+
+class Transaction(models.Model):
+    wallet = models.ForeignKey(
+        Wallet,
+        on_delete=models.CASCADE,
+        related_name="transactions",
+        verbose_name="Trxn Wallet",
+    )
+    trxn_class = models.CharField(
+        max_length=20, choices=(("debit", "DEBIT"), ("credit", "CREDIT"))
+    )
+    trxn_from = models.ForeignKey(
+        "register.customuser",
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="sender",
+    )
+    amount = models.PositiveIntegerField()
+    trxn_type = models.CharField(
+        max_length=20,
+        choices=(
+            ("withdraw", "WITHDRAW"),
+            ("transfer", "TRANSFER"),
+            ("deposit", "DEPOSIT"),
+            ("payment", "PAYMENT"),
+            ("request", "REQUEST"),
+        ),
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=(("pending", "PENDING"), ("success", "SUCCESS"), ("failed", "FAILED")),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Notification(models.Model):
+    trxn = models.ForeignKey(
+        Transaction, on_delete=models.CASCADE, related_name="notification"
+    )
+    is_read = models.BooleanField(default=False)
--- webapps2024/register/models.py
+++ webapps2024/register/models.py
@@ -1,3 +1,74 @@
+import uuid
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.core.validators import validate_email
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def validateEmail(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_("You must provide a valid email address"))
+
+    def create_superuser(self, email, name, password, *args, **kwargs):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        kwargs.setdefault("is_active", True)
+
+        if not kwargs.get("is_staff"):
+            raise ValueError(_("Super user must be assigned to is_staff=True"))
+        if not kwargs.get("is_superuser"):
+            raise ValueError(_("Super user must be assigned to is_superuser=True"))
+
+        return self.create_user(email, name, password, *args, **kwargs)
+
+    def create_user(self, email, name, password, *args, **kwargs):
+        if email:
+            email = self.normalize_email(email)
+            self.validateEmail(email)
+        else:
+            raise ValueError(_("CustomUser Account: You must provide an email address"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, *args, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    CustomUser Table
+    """
+
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_plural = "Accounts"
+
+    def email_user(self, subject, message):
+        send_mail(subject, message, "1@1.com", [self.email], fail_silently=False)
+
+    def __str__(self) -> str:
+        return self.name
