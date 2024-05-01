48eb3154d8045f094453be4aa8bb0b44f435e707
mgcrons <mgcrons@gmail.com>
2024-05-01 08:03:54 +0100

    added ui, implemented send request, transactions page

--- webapps2024/register/models.py
+++ webapps2024/register/models.py
@@ -55,7 +55,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     user_name = models.CharField(max_length=200, null=True, unique=True)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
