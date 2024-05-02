6f887b5b54d5d739d7a0aa3d95aae987247c7be5
mgcrons <mgcrons@gmail.com>
2024-05-02 18:59:41 +0100

    host error

--- webapps2024/webapps2024/settings.py
+++ webapps2024/webapps2024/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-&!d-ljm_-@1o@peos6&zo%&of41sbk=1h_$z8dz7octar*a!bx
 DEBUG = False
 
 # ALLOWED_HOSTS = ["52.91.59.127", "*"]
-ALLOWED_HOSTS = ["54.226.150.163"]
+ALLOWED_HOSTS = ["3.90.36.158"]
 AUTH_USER_MODEL = "register.customuser"
 
 # Application definition
